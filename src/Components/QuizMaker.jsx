861c4efd32be450227697b8471649a398b878871
Jaish Chimnani <jaish.chimnani@geminisolutions.com>
2022-05-26 14:28:09 +0530

    adding quiz component

--- src/Components/QuizMaker.jsx
+++ src/Components/QuizMaker.jsx
@@ -21,54 +21,123 @@ const QuizMaker = () => {
         }
     })
 
-    const questionList=formik.values.questions.map(function() {
-        return <div>
-           <input className="m-3" type="text" value={formik.values.quizTitle} onChange={formik.handleChange}/>
-               <Question />
-        </div>;   
-   })
+
+    const QuestionList = () => {
+
+    }
+
+
+
 
     function incNumberOfQuestions(i) {
         if (formik.values.questions.length < 10) {
             formik.setValues({
                 ...formik.values,
-                questions:[
+                questions: [
                     ...formik.values.questions,
                     {
-                    optionType: "",
-                    question: "",
-                    options: [""],
-                    ans: [""]
-                }]
+                        optionType: "",
+                        question: "",
+                        options: [""],
+                        ans: [""]
+                    }]
             })
-        }else{
-            document.getElementById("addQuestion").style.display="none";
+        } else {
+            document.getElementById("addQuestion").style.display = "none";
         }
     }
 
     return (
         <>
-            <ol className="text-white">
+            <form action="" onSubmit={formik.handleSubmit}>
+                <div className="text-white">
 
-                    return (
-                        <>
 
-                        <label className="m-5">Quiz Title</label>
-                        <ul className="">{questionList}</ul>
- <button id="addQuestion" onClick={(i) => incNumberOfQuestions(i)} className="btn btn-dark m-4">
-                                + Add Question</button>
-                        </>
-                    )
-                )
-            </ol>
+                    <label className="m-5">Quiz Title</label>
+                    <input className="m-3" name="quizTitle" type="text" value={formik.values.quizTitle} onChange={formik.handleChange} />
+                    {/* {alert(formik.values.quizTitle)} */}
+                    <ol className="">
+
+
+                       { formik.values.questions.map(function (item, index) {
+                    // {alert(formik.values.questions[index].question)}
+        
+        return(
+                        <div>
+                            <li className="m-5">
+
+                                <label className="m-4" htmlFor="ta-1">Question</label>
+                                <textarea name={`questions[${index}].question`}  value={formik.values.questions[index].question} onChange={formik.handleChange} className=" col-lg-12 text-white bg-transparent form-control" />
+                                <label htmlFor="" className="m-1">Options :</label>
+                                <div className="row">
+
+                                    {formik.values.questions[index].options.map((it, i) => {
+                                        return <>
+
+                                            <input name={`questions.[${index}].options[${i}]`} value={formik.values.questions[index].options[i]} onChange={formik.handleChange} className="m-5 col-lg-3 " type="text"  id="" />
+
+                                        </>
+                                    })}
+                                </div>
+                                <button className="bg-transparent" id="addOption" onClick={() => {
+                                    if (formik.values.questions[index].options.length < 5) {
+                                        const v = [...formik.values.questions];
+                                        v[index].options.push("");
+
+                                        formik.setValues({
+                                            ...formik.values,
+                                            questions: v
+                                        })
+                                    } else {
+                                        document.getElementById("addOption").style.display = "none";
+
+                                    }
+                                }}>+</button>
+
+                                <div className="row">
+
+                                    <label htmlFor="" className="col-lg-3">Correct Answers:</label>
+                                    <input type={"text"} className="col-lg-3" />
+                                </div>
+
+                                {/* <FontAwesomeIcon icon="fa-solid fa-plus" /> */}
+
+                            </li>
+                        </div>
+                        )
+            })}
+
+
+
+
+
+
+
+
+
+                    </ol>
+
+
+
+
+
+                    <button id="addQuestion" onClick={(i) => incNumberOfQuestions(i)} className="btn btn-dark m-4">
+                        + Add Question</button>
+                </div>
+
+                <div className="text-center m-5">
+                    <div className="btn btn-dark" onClick={() => {
+                        console.log(formik.values.questions);
+                    }}> Publish Quiz</div></div>
+            </form>
             {/* <div className="text-white container">
 
-        <h1>Create a new Quiz</h1>
-        quiz contains 10 questions
-        <ol>
-            <Question/>
-        </ol>
-        </div> */}
+<h1>Create a new Quiz</h1>
+quiz contains 10 questions
+<ol>
+<Question/>
+</ol>
+</div> */}
         </>
     )
 }
